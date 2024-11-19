ed40bc9e33b688b3b4154d9070e3a415fd3a4676
0rac130fD31phi <william.astley@algebraicwealth.com>
2024-11-19 15:37:07 -0500

    update

--- hive/src/main.tsx
+++ hive/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+//import PremiumHero from './PremiumHero.tsx'
+
+import LandingVariants from '../Archive/LandingVariants.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LandingVariants />
   </StrictMode>,
 )
